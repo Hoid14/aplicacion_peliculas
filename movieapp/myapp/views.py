408af8274372356e1f48e76625cf3a7e445f476b
Sergio Arias <s-andres140@hotmail.com>
2023-11-06 15:01:05 -0500

    Mostrar informacion de datos en internet 06/11/2023
    Hice una pequeña prueba para ver si estaba funcionando correctamente

---
+++ movieapp/myapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+
+# Create your views here.
+
+def users(request):
+    #pull data from third party rest api
+    response = requests.get('https://jsonplaceholder.typicode.com/users')
+
+    #convert data into json format
+    users = response.json()
+    #print(users)
+
+    return render(request, "users.html", {'users': users})
+    pass
+
